41e1fbec18c8bac7842f3ec22a1e2a7abba04177
Malthe122 <malthe122@gmail.com>
2025-02-13 15:51:36 +0100

    Initial implementation of bot winrates benchmarking

--- ScriptsOfTribute-Core/Benchmarking/BenchmarkingUtility/Utility.cs
+++ ScriptsOfTribute-Core/Benchmarking/BenchmarkingUtility/Utility.cs
@@ -10,7 +10,7 @@ using ScriptsOfTribute.AI;
 public static class Utility
 {
 
-    public static List<(AI, AI)> BuildMatchups(List<string> bots, int amount)
+    public static List<(AI, AI)> BuildMatchups(List<string> bots, int amount, bool skipExternalMatches)
     {
         List<(AI, AI)> matchups = new List<(AI, AI)>();
 
@@ -23,6 +23,15 @@ public static class Utility
                     matchups.Add((CreateBot(bots[i]), CreateBot(bots[j])));
                 }
             }
+        }
+
+        if (skipExternalMatches)
+        {
+            matchups.RemoveAll(m => 
+            {
+                return !(m.Item1 is MaltheMCTS.MaltheMCTS) && !(m.Item2 is MaltheMCTS.MaltheMCTS);
+            }
+            );
         }
 
         return matchups;
@@ -64,5 +73,70 @@ public static class Utility
                 throw new ArgumentException($"Bot '{botName}' is not recognized.");
                 // TODO add tournament bots
         }
+    }
+
+    public static string MatchResultsToCsv(Dictionary<string, Dictionary<string, int>> results, int matchesPerMatchup)
+    {
+        var bots = results.Keys.ToList();
+        var sb = new System.Text.StringBuilder();
+
+        sb.Append("vs.");
+        foreach (var bot in bots)
+        {
+            sb.Append(",").Append(bot);
+        }
+        sb.AppendLine();
+
+        foreach (var bot1 in bots)
+        {
+            sb.Append(bot1);
+            foreach (var bot2 in bots)
+            {
+                if (results[bot1].TryGetValue(bot2, out int wins))
+                {
+                    double winRate = (double)wins / matchesPerMatchup * 100.0;
+                    sb.Append(",").Append(winRate.ToString("F2"));
+                }
+                else
+                {
+                    sb.Append(",0");
+                }
+            }
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
+    }
+
+    public static string GetOverallWinRatesText(Dictionary<string, Dictionary<string, int>> data, int matches)
+    {
+        var sb = new System.Text.StringBuilder();
+
+        foreach (var bot in data.Keys)
+        {
+            int totalWins = data[bot].Values.Sum();
+            double overallWinRate = (double)totalWins / (data[bot].Count * matches) * 100.0;
+            sb.AppendLine($"{bot}: {overallWinRate}");
+        }
+
+        return sb.ToString();
+    }
+
+    public static void AddWinToResultContainer(AI winner, AI looser, Dictionary<string, Dictionary<string, int>> container)
+    {
+        string winnerName = winner.GetType().Name;
+        string looserName = looser.GetType().Name;
+
+        if (!container.ContainsKey(winnerName))
+        {
+            container.Add(winnerName, new Dictionary<string, int>());
+        }
+
+        if (!container[winnerName].ContainsKey(looserName))
+        {
+            container[winnerName].Add(looserName, 0);
+        }
+
+        container[winnerName][looserName]++;
     }
 }
