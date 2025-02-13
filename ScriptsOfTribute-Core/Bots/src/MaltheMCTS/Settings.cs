7c68985fc425bac1b3da8755caf30dd47b6e22c1
Malthe122 <malthe122@gmail.com>
2025-02-13 13:15:12 +0100

    Added possibility of simulating only ones own turn, treating it as a planning problem

--- ScriptsOfTribute-Core/Bots/src/MaltheMCTS/Settings.cs
+++ ScriptsOfTribute-Core/Bots/src/MaltheMCTS/Settings.cs
@@ -2,7 +2,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace MaltheMCTS;
 
-public class MCTSHyperparameters
+public class Settings
 {
     public double ITERATION_COMPLETION_MILLISECONDS_BUFFER { get; set; }
     public double UCT_EXPLORATION_CONSTANT { get; set; }
@@ -13,21 +13,19 @@ public class MCTSHyperparameters
     public ScoringMethod CHOSEN_SCORING_METHOD { get; set; }
     public int ROLLOUT_TURNS_BEFORE_HEURSISTIC { get; set; }
     public bool REUSE_TREE { get; set; }
+    public bool SIMULATE_MULTIPLE_TURNS { get; set; }
 
-    public MCTSHyperparameters(string filePath = "MaltheMCTSSettings")
+    public Settings()
     {
-        var config = new ConfigurationBuilder()
-        .AddIniFile(filePath, optional: false, reloadOnChange: false)
-        .Build();
-
-        ITERATION_COMPLETION_MILLISECONDS_BUFFER = config.GetValue("ITERATION_COMPLETION_MILLISECONDS_BUFFER", 100.0);
-        UCT_EXPLORATION_CONSTANT = config.GetValue("UCT_EXPLORATION_CONSTANT", 1.41421356237);
+        ITERATION_COMPLETION_MILLISECONDS_BUFFER = 500;
+        UCT_EXPLORATION_CONSTANT = 1.41421356237; // sqrt(2) generally used default value
         FORCE_DELAY_TURN_END_IN_ROLLOUT = true;
-        INCLUDE_PLAY_MOVE_CHANCE_NODES = config.GetValue("INCLUDE_PLAY_MOVE_CHANCE_NODES", false);
-        INCLUDE_END_TURN_CHANCE_NODES = config.GetValue("INCLUDE_END_TURN_CHANCE_NODES", false);
-        CHOSEN_EVALUATION_METHOD = Enum.Parse<EvaluationMethod>(config.GetValue("CHOSEN_EVALUATION_METHOD", "UCT")!);
-        CHOSEN_SCORING_METHOD = Enum.Parse<ScoringMethod>(config.GetValue("CHOSEN_SCORING_METHOD", "Rollout")!);
-        ROLLOUT_TURNS_BEFORE_HEURSISTIC = config.GetValue("ROLLOUT_TURNS_BEFORE_HEURSISTIC", 3);
+        INCLUDE_PLAY_MOVE_CHANCE_NODES = false;
+        INCLUDE_END_TURN_CHANCE_NODES = false;
+        CHOSEN_EVALUATION_METHOD = EvaluationMethod.UCT;
+        CHOSEN_SCORING_METHOD = ScoringMethod.Heuristic;
+        ROLLOUT_TURNS_BEFORE_HEURSISTIC = 1;
+        SIMULATE_MULTIPLE_TURNS = false;
         REUSE_TREE = true;
     }
 
@@ -41,6 +39,7 @@ public class MCTSHyperparameters
                 CHOSEN_EVALUATION_METHOD={CHOSEN_EVALUATION_METHOD}
                 CHOSEN_SCORING_METHOD={CHOSEN_SCORING_METHOD}
                 ROLLOUT_TURNS_BEFORE_HEURSISTIC={ROLLOUT_TURNS_BEFORE_HEURSISTIC}
+                SIMULATE_MULTIPLE_TURNS={SIMULATE_MULTIPLE_TURNS}
                 REUSE_TREE={REUSE_TREE}
                 ";
     }
