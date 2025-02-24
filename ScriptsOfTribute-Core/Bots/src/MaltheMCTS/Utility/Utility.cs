a08d06c8503a899b46629fa66d2bfcc476677f99
Malthe122 <malthe122@gmail.com>
2025-02-24 17:45:23 +0100

    Applied the new stochacity check system and fixed some things

--- ScriptsOfTribute-Core/Bots/src/MaltheMCTS/Utility/Utility.cs
+++ ScriptsOfTribute-Core/Bots/src/MaltheMCTS/Utility/Utility.cs
@@ -17,90 +17,6 @@ public static class Utility
     /// </summary>
     private const double average_bestmcts3_heuristic_score = 0.4855746429f;
     public static Random Rng = new Random();
-
-//     public static readonly List<CardId> OBVIOUS_ACTION_PLAYS = new List<CardId>(){
-//     CardId.LUXURY_EXPORTS,
-//     CardId.GOODS_SHIPMENT,
-//     CardId.MIDNIGHT_RAID,
-//     CardId.WAR_SONG,
-//     CardId.PLUNDER,
-//     CardId.TOLL_OF_FLESH,
-//     CardId.TOLL_OF_SILVER,
-//     CardId.MURDER_OF_CROWS,
-//     CardId.PILFER,
-//     CardId.SQUAWKING_ORATORY,
-//     CardId.POOL_OF_SHADOW,
-//     CardId.SCRATCH,
-//     CardId.PECK,
-//     CardId.MAINLAND_INQUIRIES,
-//     CardId.RALLY,
-//     CardId.SIEGE_WEAPON_VOLLEY,
-//     CardId.THE_ARMORY,
-//     CardId.REINFORCEMENTS,
-//     CardId.ARCHERS_VOLLEY,
-//     CardId.LEGIONS_ARRIVAL,
-//     CardId.THE_PORTCULLIS,
-//     CardId.FORTIFY,
-//     CardId.BEWILDERMENT,
-//     CardId.SWIPE,
-//     CardId.GHOSTSCALE_SEA_SERPENT,
-//     CardId.MAORMER_BOARDING_PARTY,
-//     CardId.PYANDONEAN_WAR_FLEET,
-//     CardId.SEA_ELF_RAID,
-//     CardId.SEA_SERPENT_COLOSSUS,
-//     CardId.SERPENTPROW_SCHOONER,
-//     CardId.SUMMERSET_SACKING,
-//     CardId.GOLD,
-//     CardId.WRIT_OF_COIN
-// };
-
-//     public static readonly List<CardId> OBVIOUS_AGENT_EFFECTS = new List<CardId>(){
-//     CardId.BLACKFEATHER_KNAVE,
-//     CardId.BLACKFEATHER_BRIGAND,
-//     CardId.BANNERET,
-//     CardId.KNIGHT_COMMANDER,
-//     CardId.SHIELD_BEARER,
-//     CardId.BANGKORAI_SENTRIES,
-//     CardId.KNIGHTS_OF_SAINT_PELIN,
-//     CardId.JEERING_SHADOW,
-//     CardId.PROWLING_SHADOW,
-//     CardId.STUBBORN_SHADOW,
-// };
-
-//     public static readonly List<CardId> RANDOM_ACTION_EFFECTS = new List<CardId>(){
-//         CardId.PILFER,
-//         CardId.SQUAWKING_ORATORY,
-//         CardId.THE_DREAMING_CAVE
-//     };
-//     public static readonly List<CardId> RANDOM_ACTION_COMBO_EFFECTS = new List<CardId>(){
-//         CardId.TOLL_OF_FLESH,
-//         CardId.TOLL_OF_SILVER,
-//         CardId.PILFER,
-//         CardId.SQUAWKING_ORATORY,
-//         CardId.RALLY,
-//         CardId.TWILIGHT_REVELRY
-//     };
-
-//     public static readonly List<CardId> RANDOM_CONTRACT_ACTION_EFFECTS = new List<CardId>(){
-//         CardId.HARVEST_SEASON
-//     };
-
-//     public static readonly List<CardId> RANDOM_CONTRACT_ACTION_COMBO_EFFECTS = new List<CardId>(){
-//         CardId.BLOOD_SACRIFICE,
-//         CardId.BLOODY_OFFERING,
-//         CardId.LAW_OF_SOVEREIGN_ROOST,
-//         CardId.BAG_OF_TRICKS,
-//         CardId.RINGS_GUILE,
-//     };
-
-//     public static readonly List<CardId> RANDOM_AGENT_COMBO_EFFECTS = new List<CardId>(){
-//         CardId.BLACKFEATHER_KNAVE,
-//         CardId.BLACKFEATHER_BRIGAND
-//     };
-
-//     public static readonly List<PatronId> RANDOM_PATRON_CALL_EFFECT = new List<PatronId>(){
-//         PatronId.RED_EAGLE
-//     };
     
     public static readonly List<CardId> RANDOM_EFFECT_CARDS = new List<CardId>();
 
@@ -121,36 +37,6 @@ public static class Utility
                 INSTANT_EFFECT_PLAY_CARDS.Add(card.CommonId);
             }
         }
-    }
-
-    /// <summary>
-    /// Is not taking Combo effects into account at the moment, but here we should also not look at whether the played card has an effect, but also whether there is a combo cards already played that
-    /// will be activated by playing said card
-    /// </summary>
-    /// <param name="move"></param>
-    /// <returns></returns>
-    public static bool IsNonDeterministic(this Move move)
-    {
-        return false; //TODO fix
-        // switch (move.Command)
-        // {
-        //     case CommandEnum.PLAY_CARD:
-        //         return RANDOM_ACTION_EFFECTS.Contains((move as SimpleCardMove).Card.CommonId);
-        //     case CommandEnum.ACTIVATE_AGENT:
-        //         return false; // None of these exists
-        //     case CommandEnum.ATTACK:
-        //         return false;
-        //     case CommandEnum.BUY_CARD:
-        //         return RANDOM_CONTRACT_ACTION_EFFECTS.Contains((move as SimpleCardMove).Card.CommonId);
-        //     case CommandEnum.CALL_PATRON:
-        //         return RANDOM_PATRON_CALL_EFFECT.Contains((move as SimplePatronMove).PatronId);
-        //     case CommandEnum.MAKE_CHOICE:
-        //         return false; //TODO we might add some check here too
-        //     case CommandEnum.END_TURN:
-        //         return false;
-        //     default:
-        //         return false;
-        // }
     }
 
     public static double UseBestMCTS3Heuristic(SeededGameState gameState, bool onlyEndOfTurns)
