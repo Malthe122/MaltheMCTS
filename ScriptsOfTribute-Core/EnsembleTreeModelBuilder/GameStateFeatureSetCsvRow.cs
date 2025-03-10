8709ac5352d931196a02b10ce1eda274d3d9b05f
Malthe122 <malthe122@gmail.com>
2025-03-10 18:49:19 +0100

    first attempt at building ML model

---
+++ ScriptsOfTribute-Core/EnsembleTreeModelBuilder/GameStateFeatureSetCsvRow.cs
@@ -0,0 +1,42 @@
+﻿using ScriptsOfTribute.Board.Cards;
+using ScriptsOfTribute;
+using ScriptsOfTribute.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SimpleBots.src.MaltheMCTS.Utility.HeuristicScoring.HeuristicScoring;
+using Microsoft.ML.Data;
+
+namespace EnsembleTreeModelBuilder
+{
+    public class GameStateFeatureSetCsvRow
+    {
+        [LoadColumn(0)] public int Patron_1 { get; set; }
+        [LoadColumn(1)] public int Patron_2 { get; set; }
+        [LoadColumn(2)] public int Patron_3 { get; set; }
+        [LoadColumn(3)] public int Patron_4 { get; set; }
+        [LoadColumn(4)] public int CurrentPlayerPrestige { get; set; }
+        [LoadColumn(5)] public double CurrentPlayerDeck_PrestigeStrength { get; set; }
+        [LoadColumn(6)] public double CurrentPlayerDeck_PowerStrength { get; set; }
+        [LoadColumn(7)] public double CurrentPlayerDeck_GoldStrength { get; set; }
+        [LoadColumn(8)] public double CurrentPlayerDeck_MiscStrength { get; set; }
+        [LoadColumn(9)] public double CurrentPlayerDeckComboProportion { get; set; }
+        [LoadColumn(10)] public double CurrentPlayerAgent_PowerStrength { get; set; }
+        [LoadColumn(11)] public double CurrentPlayerAgent_GoldStrength { get; set; }
+        [LoadColumn(12)] public double CurrentPlayerAgent_MiscStrength { get; set; }
+        [LoadColumn(13)] public int CurrentPlayerPatronFavour { get; set; }
+        [LoadColumn(14)] public int OpponentPrestige { get; set; }
+        [LoadColumn(15)] public double OpponentDeck_PrestigeStrength { get; set; }
+        [LoadColumn(16)] public double OpponentDeck_PowerStrength { get; set; }
+        [LoadColumn(17)] public double OpponentDeck_GoldStrength { get; set; }
+        [LoadColumn(18)] public double OpponentDeck_MiscStrength { get; set; }
+        [LoadColumn(19)] public double OpponentAgent_PowerStrength { get; set; }
+        [LoadColumn(20)] public double OpponentAgent_GoldStrength { get; set; }
+        [LoadColumn(21)] public double OpponentAgent_MiscStrength { get; set; }
+        [LoadColumn(22)] public int OpponentPatronFavour { get; set; }
+        [LoadColumn(23), ColumnName("Label")] public double WinProbability { get; set; }
+    }
+
+}
--- ScriptsOfTribute-Core/EnsembleTreeModelBuilder/Program.cs
+++ ScriptsOfTribute-Core/EnsembleTreeModelBuilder/Program.cs
@@ -3,11 +3,19 @@ using CsvHelper;
 using System.CommandLine;
 using System.Globalization;
 using SimpleBots.src.MaltheMCTS.Utility.HeuristicScoring;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Microsoft.ML;
+using Microsoft.ML.AutoML;
+using Microsoft.ML.Trainers.LightGbm;
+using Tensorflow;
+using System.Text;
 
 namespace EnsembleTreeModelBuilder
 {
     internal class Program
     {
+        public const string MODELS_FOLDER = "GeneratedModels";
+
         static async Task Main(string[] args)
         {
             var TrainingDataFileOption = new Option<string?>(
@@ -20,34 +28,94 @@ namespace EnsembleTreeModelBuilder
                description: "Name for saving the built model"
             );
 
+            var ExperiementTimeOption = new Option<uint>(
+               aliases: new[] { "--experiementTime", "-et" },
+               description: "Time for model experiementation"
+            );
+
             var rootCommand = new RootCommand
             {
                 TrainingDataFileOption,
                 ModelNameOption,
+                ExperiementTimeOption
             };
 
             var arguments = rootCommand.Parse(args);
 
             rootCommand.SetHandler(
                 TrainModel,
-                TrainingDataFileOption, ModelNameOption
+                TrainingDataFileOption, ModelNameOption, ExperiementTimeOption
             );
 
             await rootCommand.InvokeAsync(args);
-
         }
 
-        private static void TrainModel(string trainingDataFilePath, string modelName)
+        private static void TrainModel(string trainingDataFilePath, string modelName, uint experiementTime)
         {
-            var trainingFeatureSets = LoadCsvData(trainingDataFilePath);
+
+            Directory.CreateDirectory(MODELS_FOLDER + "/" + modelName);
+
+            var mlContext = new MLContext();
+
+            //Console.WriteLine("Loading data from: '" + trainingDataFilePath + "'...");
+            //var dataSets = mlContext.Data.LoadFromTextFile<GameStateFeatureSetCsvRow>(
+            //path: trainingDataFilePath, hasHeader: true, separatorChar: ';');
+            //Console.WriteLine("Loaded training data with " + dataSets.Schema.Count + " collumns");
+
+            var columnInference = mlContext.Auto().InferColumns(trainingDataFilePath, labelColumnName: "WinProbability", groupColumns: false);
+            IDataView fullData = mlContext.Data.LoadFromTextFile(trainingDataFilePath, columnInference.TextLoaderOptions);
+
+            var trainTestSplit = mlContext.Data.TrainTestSplit(fullData, testFraction: 0.2);
+            IDataView trainData = trainTestSplit.TrainSet;
+            IDataView testData = trainTestSplit.TestSet;
+
+            string labelName = columnInference.ColumnInformation.LabelColumnName;
+            var pipeline = mlContext.Auto().Featurizer(trainData, columnInformation: columnInference.ColumnInformation)
+                .Append(mlContext.Auto().Regression(
+                    labelColumnName: labelName,
+                    // Here i can allow only one type, e.g. FastForest if i want faster inference time in exchange for lower accuracy
+                    useFastForest: true,
+                    useFastTree: true,
+                    useLbfgsPoissonRegression: true,
+                    useSdca: true,
+                    useLgbm: true
+            ));
+
+            var experiementSettings = new RegressionExperimentSettings
+            {
+                MaxExperimentTimeInSeconds = experiementTime,
+            };
+
+            var experiment = mlContext.Auto().CreateExperiment();
+            experiment.SetPipeline(pipeline)
+                      .SetRegressionMetric(RegressionMetric.RSquared, labelColumn: labelName)
+                      .SetDataset(trainData, testData);
+
+            Console.WriteLine("Running AutoML experiment...");
+            var experimentResult = experiment.RunAsync().GetAwaiter().GetResult();
+            var bestModel = experimentResult.Model;
+            double bestRSquared = experimentResult.Metric;  // Best R² on validation (test) set
+
+            // Save the model to a .zip file for later use
+            mlContext.Model.Save(bestModel, trainData.Schema, MODELS_FOLDER + modelName + "/" + modelName + ".zip");
+
+            string bestPipeline = pipeline.ToString(experimentResult.TrialSettings.Parameter);
+            StringBuilder info = new StringBuilder();
+            info.AppendLine("R²:" + bestRSquared);
+            info.AppendLine();
+            info.AppendLine("Pipeline details:");
+            info.AppendLine(bestPipeline);
+            File.WriteAllText(MODELS_FOLDER + modelName + "/" + "Details.txt", info.ToString());
+
+            Console.WriteLine("Best model saved in " + MODELS_FOLDER + modelName);
         }
 
-        private static List<GameStateFeatureSet> LoadCsvData(string filePath)
-        {
-            using var reader = new StreamReader(filePath);
-            using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = true });
+        //private static List<GameStateFeatureSetCsvRow> LoadCsvData(string filePath)
+        //{
+        //    using var reader = new StreamReader(filePath);
+        //    using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = true });
 
-            return new List<GameStateFeatureSet>(csv.GetRecords<GameStateFeatureSet>());
-        }
+        //    return new List<GameStateFeatureSetCsvRow>(csv.GetRecords<GameStateFeatureSetCsvRow>());
+        //}
     }
 }
