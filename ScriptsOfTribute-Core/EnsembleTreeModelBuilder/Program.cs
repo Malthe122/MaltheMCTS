d9b64aca455e248ea1348968507a715b42971ea5
Malthe122 <malthe122@gmail.com>
2025-03-13 13:14:39 +0100

    Revert "make sure that Ubuntu has access to the folder it creates"
    This reverts commit 7d7919a3316859bd3a4409830606918d19a81a5b.

--- ScriptsOfTribute-Core/EnsembleTreeModelBuilder/Program.cs
+++ ScriptsOfTribute-Core/EnsembleTreeModelBuilder/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.ML.Trainers.LightGbm;
 using Tensorflow;
 using System.Text;
 using static Microsoft.ML.AutoML.AutoMLExperiment;
-using System.Security.AccessControl;
 
 namespace EnsembleTreeModelBuilder
 {
@@ -55,16 +54,7 @@ namespace EnsembleTreeModelBuilder
 
             Console.WriteLine("Running training on " + trainingDataFilePath + "...");
 
-            // This is needed since Ubuntu creates the folder without rights for the application to add files to it in the end
-            var directory = Directory.CreateDirectory(MODELS_FOLDER + "/" + modelName);
-            DirectorySecurity security = directory.GetAccessControl();
-            security.AddAccessRule(new FileSystemAccessRule("Everyone",
-                FileSystemRights.FullControl,
-                InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit,
-                PropagationFlags.None,
-                AccessControlType.Allow));
-            directory.SetAccessControl(security);
-
+            Directory.CreateDirectory(MODELS_FOLDER + "/" + modelName);
 
             //CleanIntegersFromCsv(trainingDataFilePath); // Check if this is neccessary or puttin the properties as floats in class is enough
 
